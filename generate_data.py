87e14b6cc4b5b1cd4fe543557cb5640fff394b25
WilliamSongLiu <williamsongliu@gmail.com>
2024-03-12 15:52:20 -0700

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -30,8 +30,17 @@ for i in range(1):
         board[unoccupied_position[0]][unoccupied_position[1]] = j
         positions.append(unoccupied_position)
 
-    teammates = "".join([f"{character_names[j]}\n" if teams[j] == teams[my_index] else "" for j in range(num_characters)])
-    enemies = "".join([f"{character_names[j]}\n" if teams[j] != teams[my_index] else "" for j in range(num_characters)])
+    board_str = ""
+    for k in range(board_size):
+        for j in range(board_size):
+            if board[j][k] == None:
+                board_str += "-"
+            else:
+                board_str += character_names[board[j][k]][0]
+        board_str += "\n"
+
+    teammates = "".join([f"{character_names[j]} (health: {random.randint(1, 10)}, damage: {random.randint(1, 3)})\n" if teams[j] == teams[my_index] else "" for j in range(num_characters)])
+    enemies = "".join([f"{character_names[j]} (health: {random.randint(1, 10)}, damage: {random.randint(1, 3)})\n" if teams[j] != teams[my_index] else "" for j in range(num_characters)])
 
     actions = []
     for j in range(positions[my_index][0] - 1, positions[my_index][0] + 2):
@@ -47,52 +56,39 @@ for i in range(1):
 
     moves = "".join([f"{j} : I can move to {actions[j][1]}\n" if actions[j][0] == "M" else "" for j in range(len(actions))])
     attacks = "".join([f"{j} : I can attack {character_names[board[actions[j][1][0]][actions[j][1][1]]]} at {actions[j][1]}\n" if actions[j][0] == "A" else "" for j in range(len(actions))])
+    actions_str = "".join([f"{j} : I can move to {actions[j][1]}\n" if actions[j][0] == "M" else f"{j} : I can attack {character_names[board[actions[j][1][0]][actions[j][1][1]]]} at {actions[j][1]}\n" for j in range(len(actions))])
 
-    print(f"num_characters {num_characters}")
-    print(f"teams {teams}")
-    print(f"my_index {my_index}")
-    print(f"board_size {board_size}")
-    print(f"positions {positions}")
-    print()
+    turn_order = [character_names[j] for j in range(num_characters)]
+    random.shuffle(turn_order)
+    turn_order_str = "\n".join(turn_order)
 
-    data = f"""I am {character_names[my_index]} I am represented by {character_names[my_index][0]}
-I am on team {teams[my_index]}
+    # print(f"num_characters {num_characters}")
+    # print(f"teams {teams}")
+    # print(f"my_index {my_index}")
+    # print(f"board_size {board_size}")
+    # print(f"positions {positions}")
+    # print()
+
+    data = f"""I am {character_names[my_index]} represented by the letter {character_names[my_index][0]}
 I have {random.randint(1, 10)} health
-I can do 1 damage
-I can move 1 tiles
-My attack range is 1 tiles
+I can do {random.randint(1, 3)} damage per turn
+I can move 1 tile per turn
+My attack range is 1 tile
 I am at position {positions[my_index]}
 
-My teammates are: (team {teams[my_index]})
+My teammates are:
 {teammates}
-My enemies are: (team {0 if teams[my_index] == 1 else 1})
+My enemies are:
 {enemies}
 My valid moves are:
 {moves}
 My valid attacks are:
 {attacks}
 The board looks like:
-A - - - -
-- B - - -
-- - C - D
-- - - - -
-- - - - E
-
+{board_str}
 The turn order is:
-Charlie
-Alice
-Eve
-Bob
-David
+{turn_order_str}
 
 Choose an action:
-0 : I can move to (1, 1)
-1 : I can move to (1, 2)
-2 : I can move to (1, 3)
-3 : I can move to (2, 1)
-4 : I can move to (2, 3)
-5 : I can move to (3, 1)
-6 : I can move to (3, 2)
-7 : I can move to (3, 3)
-8 : I can Attack Bob at (1, 1)"""
+{actions_str}"""
     print(data)
